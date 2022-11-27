544eb2ad5f42b814c397d36e3ec3f5afedafabb4
Accoder <emscleto11@gmail.com>
2022-11-27 21:29:22 +0800

    create myscript.js

---
+++ activity5/myscript.js
@@ -0,0 +1,43 @@
+
+var length = 3;
+var width = 4;
+var height = 5;
+var shape = "square";
+var color = "yellow";
+var Sum = 0;
+var Product = 0;
+var Modulo = 0;
+
+
+console.log("Sum of length, width, and height  = ",
+Sum = length + width + height);
+
+console.log("Product of length and width = ", Product = length * width);
+
+console.log("Modulo of length and height = ", 
+Modulo = length % width);
+
+console.log("Sum of shape and length  = ",
+Sum = shape + length);
+
+console.log("Sum of shape and color  = ",
+Sum = shape + color);
+
+console.log("Sum of color, length, and height  = ",
+Sum = color + (length + height));
+
+console.log("Sum of width, height, and shape  = ",
+Sum = width + height + shape);
+
+
+// Use a looping statement to add all numbers from 1 to 100 and output the sum
+
+var sum = 0;
+
+for (var i = 1; i < 101; i++) {
+    if (i > 0);
+    sum += i;
+    console.log(sum);
+}
+
+
