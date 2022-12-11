17d73b4be68c59a55dba8ed97e37b28ccb517f3c
Accoder <emscleto11@gmail.com>
2022-12-11 19:14:27 +0800

    create folders

---
+++ sample files/palindrome.js
@@ -0,0 +1,13 @@
+// var word = prompt("Enter a word");
+function palindrome(word) {
+var palindrome = apple;
+// var palindrome = word;
+// var h1 = document.getElementById("");
+// h1.innerHTML = "Yes," + word + "is a palindrome"
+
+console.log(word);
+console.log(word.length);
+for (var i = word.length; i > 0; i--) {
+    console.log(i);
+}
+}
